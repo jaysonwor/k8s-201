10896d4fbd2bdd747c680bd9a82566a6db55f723
Jayson Workman <jaysonw@netapp.com>
2018-06-16 02:59:35 +0000

    fix delete

--- delete.sh
+++ delete.sh
@@ -3,4 +3,4 @@ echo "deletes the cluster"
 echo "cluster FQDN: $CLUSTER_FQDN"
 echo "s3 bucket: $BUCKET_NAME"
 
-kops create cluster --name=$CLUSTER_FQDN --state=s3://$BUCKET_NAME
+kops delete cluster --name=$CLUSTER_FQDN --state=s3://$BUCKET_NAME --yes
